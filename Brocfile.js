307e8d0bb404f91f7f27ccbe2302fd9d8fc84df6
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-03-01 22:56:07 -0800

    Simplifying Brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -4,20 +4,13 @@ var pickFiles   = require('broccoli-static-compiler');
 var compileLess = require('broccoli-less-single');
 var mergeTrees  = require('broccoli-merge-trees');
 
-var appTree = pickFiles('app', {
+var app = pickFiles('app', {
 	srcDir:  '/',
 	destDir: '/'
 });
 
-var bootstrapTree = pickFiles('bower_components', {
-	srcDir:  '/',
-	destDir: 'bower_components'
-});
-
-var app = mergeTrees([appTree, bootstrapTree]);
-
-var lessTree = compileLess(app, 'styles/app.less', 'assets/app.css', {
+var less = compileLess(app, 'styles/app.less', 'assets/app.css', {
 	paths: ['.', 'bower_components/bootstrap/less']
 })
 
-module.exports = mergeTrees([app, lessTree]);
+module.exports = mergeTrees([app, less]);
